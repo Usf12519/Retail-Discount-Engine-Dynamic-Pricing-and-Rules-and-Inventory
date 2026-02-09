5e21bc1a357eb29a74f2d3153dd11b234b9a160a
Cameron Smith <Cs11@usf.edu>
2026-02-09 13:42:26 -0500

    Created array of objects including 5 products

---
+++ cc_4.js
@@ -0,0 +1,11 @@
+// Step 2: Creating Array of Objects including 5 Products
+
+const products = [
+    { name: "Eyeliner", category: "Cosmetics", price: 55, inventory: 100 },
+    { name: "Mascera", category: "Costmetics", price: 80, inventory: 150 },
+    { name: "Lipstick", category: "Cosmetics", price: 100, inventory: 35 },
+    { name: "Shaving Cream", category: "Cosmetics", price: 40, inventory: 60 },
+    { name: "Beard Oil", category: "Cosmetics", price: 33, inventory: 20 },
+];
+
+// Step 3: 
